c2063962b40d3fb3201b39641e24e9936f3a3813
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-05-08 15:42:36 +0800

    testing for deployment

--- public/js/app-frontend.js
+++ public/js/app-frontend.js
@@ -39,6 +39,9 @@ import appLogoPath from '../img/cratz-pad-app-logo.png';
 import loaderImgPath from '../img/loader.gif';
 import windowsLogoPath from '../img/windows-logo.png';
 import appleLogoPath from '../img/apple-logo.png';
+import sheetApplePath from '../img/sheets/sheet_apple_64_indexed_128.png';
+import sheetGooglePath from '../img/sheets/sheet_google_64_indexed_128.png';
+import sheetTwitterPath from '../img/sheets/sheet_twitter_64_indexed_128.png';
 
 // Own Utility Modules
 import * as ButtonsEditor from '../../util/editor-buttons-utils';
@@ -165,10 +168,9 @@ document.addEventListener('DOMContentLoaded', () => {
         positioning: 'vertical',
         use_sheets: true,
         sheets: {
-            apple: '/sheets/sheet_apple_64_indexed_128.png',
-            google: '/sheets/sheet_google_64_indexed_128.png',
-            twitter: '/sheets/sheet_twitter_64_indexed_128.png',
-            emojione: '/sheets/sheet_emojione_64_indexed_128.png',
+            apple: sheetApplePath,
+            google: sheetGooglePath,
+            twitter: sheetTwitterPath,
         },
         categories: [
             {
