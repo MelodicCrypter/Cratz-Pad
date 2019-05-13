623b63cc69efcb1888ebd42fb902c1cca8e06b78
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-05-13 23:48:34 +0800

    modified to find speficic files

--- controllers/pages/home-controller.js
+++ controllers/pages/home-controller.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const gm = require('getmac');
+const find = require('find');
 
 // Get user's mac address
 // this will be used for naming unique localStorage key
@@ -12,6 +13,40 @@ gm.getMac((err, macAddress) => {
     vitalData = macAddress;
 });
 
+// This will get the frontend.bundle.js files
+let appJS;
+let vendorsJS;
+let runtimeJS;
+find
+    .file(/(?:^|\W)app(?:$|\W)/, path.join(__dirname, '../../public/dist/'), (files) => {
+        appJS = files[0].replace('public/dist/', '');
+    })
+    .error((err) => {
+        if (err) {
+            //console.log(err);
+        }
+    });
+
+find
+    .file(/(?:^|\W)runtime(?:$|\W)/, path.join(__dirname, '../../public/dist/'), (files) => {
+        runtimeJS = files[0].replace('public/dist/', '');
+    })
+    .error((err) => {
+        if (err) {
+            //console.log(err);
+        }
+    });
+
+find
+    .file(/(?:^|\W)vendors(?:$|\W)/, path.join(__dirname, '../../public/dist/'), (files) => {
+        vendorsJS = files[0].replace('public/dist/', '');
+    })
+    .error((err) => {
+        if (err) {
+            //console.log(err);
+        }
+    });
+
 // Controller for your homepage or index
 const renderHome = (req, res) => {
     res.render('index', {
@@ -20,6 +55,9 @@ const renderHome = (req, res) => {
             pageTitle: 'Cratz Pad | Write All You Want',
             title: 'Cratz Pad',
             vital: vitalData.toString(),
+            appJS,
+            runtimeJS,
+            vendorsJS,
         },
         partials: {
             header: path.resolve('views/partials/header.html'),
